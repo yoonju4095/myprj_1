0ca5625539547d6c5ec992b2922903433cd83282
yoonju <yoonju9311@naver.com>
2023-03-13 15:14:56 +0900

    삭제 기능 추가

--- test.js
+++ test.js
@@ -2,3 +2,8 @@ const $btnAdd = document.getElementById('btnAdd');
 $btnAdd.addEventListener('click', () => {
   console.log('클릭');
 });
+
+const $btnDel = document.getElementById('btnDel');
+$btnAdd.addEventListener('click', () => {
+  console.log('삭제');
+});
